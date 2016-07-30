883fa41e52575df70cc176e0c0a28ce0c9f59d5f
giacomociti <giacomociti@gmail.com>
2016-07-31 01:11:18 +0200

    readme updated

--- src/FSharp.Data.Xsd/AssemblyInfo.fs
+++ src/FSharp.Data.Xsd/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.Xsd")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.Xsd")>]
 [<assembly: AssemblyDescriptionAttribute("Xml Type Provider based on Xml Schema")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] InformationalVersion = "0.0.1"
